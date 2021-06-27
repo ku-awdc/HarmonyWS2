d2b40040ff1b8d0b41f13dfea04714cbf6b4c3b8
Matt Denwood <md@sund.ku.dk>
2021-06-27 11:53:36 +0200

    Minor tweaks to sessions 1-5

---
+++ rsc/setup.R
@@ -0,0 +1,56 @@
+## TODO:
+
+# move general setup code here so it is re-used
+# use start/end functions
+# set working directory and auto-cleanup anything created
+
+library("tidyverse")
+library("runjags")
+library("knitr")
+
+set.seed(2021-06-22)
+
+# Reduce the width of R code output for PDF only:
+if(params$presentation) options(width=60)
+knitr::opts_chunk$set(echo = TRUE)
+
+# Reduce font size of R code output for Beamer:
+if(params$presentation){
+  knitr::knit_hooks$set(size = function(before, options, envir) {
+    if(before){
+      knitr::asis_output(paste0("\\", options$size))
+    }else{
+      knitr::asis_output("\\normalsize")
+    }
+  })
+  knitr::opts_chunk$set(size = "scriptsize")
+}
+
+# Collapse successive chunks:
+space_collapse <- function(x){ gsub("```\n*```r*\n*", "", x) }
+# Reduce space between chunks:
+space_reduce <- function(x){ gsub("```\n+```\n", "", x) }
+knitr::knit_hooks$set(document = space_collapse)
+
+# To collect temporary filenames:
+cleanup <- character(0)
+
+exercise_start <- function(exercise_current=exercise_current){
+	rv <- ""
+	if(params$presentation & !exercise_current){
+		rv <- "\\begin{comment}"
+		exercise_current <<- TRUE
+	}
+	return(rv)
+}
+
+exercise_end <- function(exercise_status=exercise_status){
+	rv <- ""
+	if(params$presentation & exercise_current){
+		rv <- "\\end{comment}"
+		exercise_current <<- FALSE
+	}
+	return(rv)
+}
+
+exercise_current <- FALSE
